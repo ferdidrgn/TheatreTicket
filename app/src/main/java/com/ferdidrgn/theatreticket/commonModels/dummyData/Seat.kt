3a4400d3283d5026b5bf1122e05c7975de63f08f
ferdi <ferdidurgun34@gmail.com>
2023-11-08 05:49:56 +0300

    add show detaisl ui

---
+++ app/src/main/java/com/ferdidrgn/theatreticket/commonModels/dummyData/Seat.kt
@@ -0,0 +1,19 @@
+package com.ferdidrgn.theatreticket.commonModels.dummyData
+
+import com.ferdidrgn.theatreticket.base.ListAdapterItem
+
+data class Seat(
+    var _createdAt: String? = null,
+    var _id: String? = null,
+    var _statu: Boolean?,
+    var no: String? = null,
+    var isSelected: Boolean? = null,
+
+    override val mId: Long = 1L
+) : ListAdapterItem {
+
+    //took isSelected for marking one item as selected or not
+    fun setIsSelected(value: Boolean) {
+        isSelected = value
+    }
+}
